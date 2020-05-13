15a244963ce894367428910b46df8bfa7e089430
Fajrul Falah <ffajrul55@gmail.com>
2020-05-13 19:32:07 +0700

    Add files via upload

---
+++ make.py
@@ -0,0 +1,14 @@
+from title import create_title
+
+#read the txt file
+myfile = open('text.txt', 'r')
+title = myfile.readlines()
+
+#define variable
+count = 0
+
+#loop for creating bunch of images from txt file
+for x in title:
+    count += 1
+    name = 'example' + str(count) + '.png'
+    create_title(x,name)
